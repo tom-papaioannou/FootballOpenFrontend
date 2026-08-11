323a386589e458caded49abb1c6905d9421adc42
Tom Papaioannou <tom-papaioannou@outlook.com>
2026-08-11 21:36:49 +0300

    Add shapes to kits

--- src/app/components/team-kit/team-kit.ts
+++ src/app/components/team-kit/team-kit.ts
@@ -5,6 +5,7 @@
 
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Kit, KitShape } from '../../models/competition.model';
 
 @Component({
   selector: 'app-team-kit',
@@ -16,6 +17,52 @@ import { Component, Input } from '@angular/core';
   standalone: true
 })
 export class TeamKit {
-  @Input() kit: any;
+  @Input() kit?: Kit | null;
   @Input() isHome: boolean = true;
+
+  get patternClass(): string | null {
+    const kitShape = this.normalizedKitShape;
+
+    switch (kitShape) {
+      case KitShape.StripedFiveLines:
+        return 'shirt-pattern-striped-five';
+      case KitShape.StripedSevenLines:
+        return 'shirt-pattern-striped-seven';
+      case KitShape.DiagonalStripe:
+        return 'shirt-pattern-diagonal';
+      case KitShape.HalfAndHalf:
+        return 'shirt-pattern-half';
+      default:
+        return null;
+    }
+  }
+
+  private get normalizedKitShape(): KitShape {
+    const kitShape = this.kit?.kitShape;
+
+    if (typeof kitShape === 'number') {
+      return kitShape;
+    }
+
+    if (typeof kitShape !== 'string') {
+      return KitShape.Empty;
+    }
+
+    switch (kitShape.toLowerCase()) {
+      case '1':
+      case 'stripedfivelines':
+        return KitShape.StripedFiveLines;
+      case '2':
+      case 'stripedsevenlines':
+        return KitShape.StripedSevenLines;
+      case '3':
+      case 'diagonalstripe':
+        return KitShape.DiagonalStripe;
+      case '4':
+      case 'halfandhalf':
+        return KitShape.HalfAndHalf;
+      default:
+        return KitShape.Empty;
+    }
+  }
 }
